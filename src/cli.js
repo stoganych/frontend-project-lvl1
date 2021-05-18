9ce905b48ab9c39f0bd0846a87e6b7ef5139f71d
Timofey Ludvenko <stoganych@gmail.com>
2021-05-18 13:07:01 +0300

    fix style code with eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
-export const welcomeUser = () => {
-   const userName = readlineSync.question('May I have your name? ');
-   return console.log(`Hello, ${userName}!`);
+const welcomeUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello, ${userName}!`);
 };
+
+export default welcomeUser;
