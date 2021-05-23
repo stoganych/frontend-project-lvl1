1ea35cc657d8ae7fbe520fae5858e085e225de21
Timofey Ludvenko <stoganych@gmail.com>
2021-05-23 18:11:13 +0300

    create new func of chose the operator

--- src/games/calcNumber.js
+++ src/games/calcNumber.js
@@ -6,21 +6,38 @@ const getRandomOperator = () => {
   return operators[Math.floor(Math.random() * 3)];
 };
 
+const solveTheExpression = (oper1, oper2, operand) => {
+  let result;
+  switch (operand) {
+    case '+':
+      result = oper1 + oper2;
+      break;
+    case '-':
+      result = oper1 - oper2;
+      break;
+    case '*':
+      result = oper1 * oper2;
+      break;
+    default:
+      console.log('no such operator');
+  }
+  return result;
+};
+
 const playGame = () => {
   const userName = gameLogic.welcomeUser();
-  console.log(`Hello, ${userName}!`);
+  gameLogic.sayHello(userName);
   console.log('What is the result of the expression?');
   for (let i = 0; i <= 2; i++) {
     const firstOperand = gameLogic.getRandomInt();
     const secondOperand = gameLogic.getRandomInt();
     const operator = getRandomOperator();
     console.log(`Question: ${firstOperand} ${operator} ${secondOperand}`);
-    const expression = eval(`${firstOperand} ${operator} ${secondOperand}`);
+    const expression = solveTheExpression(firstOperand, secondOperand, operator);
     const answer = +readlineSync.question('Your answer: ');
     if (expression === answer) console.log('Correct!');
     else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expression}'`);
-      return console.log(`Let's try again, ${userName}`);
+      return gameLogic.getLoseMessage(answer, expression, userName);
     }
   }
   return gameLogic.congratulateOn(userName);
