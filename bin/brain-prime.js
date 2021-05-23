bd5d2a1eac8d79b0b48c33b444a52cf27e984651
Timofey Ludvenko <stoganych@gmail.com>
2021-05-23 23:35:26 +0300

    new game

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import playGame from '../src/games/primeNumber.js';
+
+playGame();
---
+++ src/games/primeNumber.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+import * as gameLogic from '../index.js';
+
+const getTryAnswer = (num) => {
+  if (num === 1) return 'yes';
+  if (num === 2) return 'no';
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return 'no';
+  }
+  return 'yes';
+};
+
+const playGame = () => {
+  const userName = gameLogic.welcomeUser();
+  gameLogic.sayHello(userName);
+  gameLogic.outputRules('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i <= 2; i++) {
+    const randomNum = gameLogic.getRandomInt(100);
+    gameLogic.sayQuestion([randomNum]);
+    const answer = readlineSync.question('Your answer: ');
+    if (getTryAnswer(randomNum) === answer) console.log('Correct!');
+    else {
+      return gameLogic.getLoseMessage(answer, getTryAnswer(randomNum), userName);
+    }
+  }
+  return gameLogic.congratulateOn(userName);
+};
+
+export default playGame;
