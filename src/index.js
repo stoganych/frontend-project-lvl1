f77eb0c88b6426d345b0f0b5a75cfa906f58369a
Timofey Ludvenko <stoganych@gmail.com>
2021-05-23 22:23:45 +0300

    fix func welcomeUser

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync';
 export const welcomeUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
+  if (userName === '') return 'Anonim';
   return userName;
 };
 
